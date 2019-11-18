7052f8c73b7d7cb0325829d103ac61cbd32a4c23
Edo <E.Costantini@tilburguniversity.edu>
2019-11-18 16:18:51 +0100

    refinements to try_CART_impute

--- R/try_CART_impute.R
+++ R/try_CART_impute.R
@@ -67,13 +67,13 @@ library(bayesboot)
     # Output: "class" / "anova" (this defines whether we need to grow a classification or a regression tree, respectively)
   ## Trial inputs
     # x <- mtcars$mpg # continuous example
-    # x <- mtcars$am  # dichotomous example
-    if(length(unique(x)) == 2) {type <- "class"} else {type <- "anova"}
+    # x <- as.factor(mtcars$am)  # dichotomous example
+    if(is.factor(x)) {type <- "class"} else {type <- "anova"}
     return(type)
   }
   
 # Bayesian Bootstrap
-  bbootstrap <- function(x, size) {
+  bbootstrap <- function(x) {
     # based on Rubin 1998
   ## Description
     # Input: 
@@ -82,7 +82,7 @@ library(bayesboot)
     # Output: a bayesian bootsrap sample of size size of x
   ## Trial inputs
     # x <- rbinom(n, 1, .5)
-    # size <- 10
+    size <- length(x)
     u <- sort(c(runif(length(x)-1, 0, 1))) # n-1 uniform draws
     g <- numeric(0)
     for (i in 1:(length(x))) {
@@ -102,7 +102,7 @@ library(bayesboot)
   }
   
 # Single Imputation function
-  CARTimpute <- function(dt_i, iter, p_imod) {
+  CARTimpute <- function(dt_i, iters, p_imod) {
   ## Description
     # Input: 
     # - dt_i: a dataset with missing values
@@ -110,9 +110,15 @@ library(bayesboot)
     # - p_imod: number of relevant predictors for imputation model (needed for stochastic regression for initial values)
     # Output: one imputed dataset
   ## Trial inputs
-    # dt_i <- airquality #dt[[2]] # with missings mice::md.pattern(airquality)
+    # dt_i <- airquality # with missings mice::md.pattern(airquality)
     # iter <- 10
     # p_imod <- ncol(dt_i) # for low dimensional data this is enough 
+    # k <- 1 # just for running the inside of the loop
+  ## Notes: right now this implementation differes from Doove et al 2014 in that: (a) initialization of 
+    # missing values is done with conditional mean imputation instead of random draws from the observed
+    # relevant values; (B) I implemented a bayesian bootstrap resampling of the donor set following 
+    # Burgette Rieter 2010; (C) I need to look more into the specification of a CART (do I need to define
+    # the value of "cp")
     
     K <- sum(is.na(colSums(dt_i, na.rm = FALSE))) # number of variables needing imputation
     K_names <- names(which(colSums(apply(dt_i, 2, is.na)) != 0)) # select the names of these k variables
@@ -123,53 +129,80 @@ library(bayesboot)
     #   followed here, and since I'm following the approach of Reiter and similar
     #   papers that are focused on creating sythetic datasets, there is not a
     #   specific guideline on what to do in this approach.
+    print("Data initialization by MICE stochastic regression")
     imp <- mice::mice(dt_i[,1:p_imod], method = "norm.nob", m = 1)
-    dt_inizialised <- cbind(mice::complete(imp)[, names(dt_i) %in% K_names], dt_i[, !names(dt_i) %in% K_names])
+    dt_init <- cbind(mice::complete(imp)[, names(dt_i) %in% K_names], dt_i[, !names(dt_i) %in% K_names])
     
-    dt_prev <- dt_inizialised # this data will be the previous dataset at the beginning of the loop and the new data at the end
+    dt_aug <- dt_init # this data will be the previous dataset at the beginning of the loop and the new data at the end
     
-    for(iter in 1:10) {
+    for(i in 1:iters) {
+      print(paste0("Iteration status: ", i, " of ", iters))
       for (k in 1:K) {
+        print(paste0("Variable under imputation: ", K_names[k], " (", k, " of ", K, ")" ))
         # Prep Data
-        indx_k <- names(dt_prev) %in% K_names[k]               # to index x_k
-        data4tree <- arrange_dt_x_k(dt_prev, K_names[k])
+        indx_k <- names(dt_aug) %in% K_names[k]               # to index x_k
+        ry <- !is.na(dt_i[, K_names[k]])                      # response variable T = observed, F = NA
+        data4tree <- arrange_dt_x_k(dt_aug, K_names[k])[ry, ] # arrange and select Xobs (values of Xno_k for which X_k is observed)
         
         # Define tree type (classification or regression) based on nature of variable under imputation
         tt <- tree_type(data4tree$x_k)
         
         # Grow tree
-        tree <- rpart(x_k ~., 
-                      data = data4tree, 
-                      method = tt)
-
-        # Identify terminal nodes (l leafs) predictions (costum)
-        leafs <- data.frame(x_k = dt_prev[, indx_k],
-                            lf = tree$where)        # terminal nodes
-        leaf_pred <- numeric(0)
-        for (l in 1:length(unique(tree$where))) {
-          x <- leafs[leafs$lf == unique(tree$where)[l] ,1] # for each terminal node, exctract the values of x_k in there
-          bbx <- bbootstrap(x, length(x))                  # take a bootstrap sample of these values
-          leaf_pred[l] <- sample(bbx, 1)                   # and sample 1 value from this conditional distribution
-                                                           # (doubt: is a simple random sample with every value having equal prob enough?)
-        }
-        imputation_frame <- data.frame(leaf_id = unique(tree$where), # what value should be imputed if you fall in a node 
-                                       leaf_pred)
+        tree <- rpart(x_k ~., data = data4tree, method = tt)
         
-        # Identify Xno_k values corresponding to the missing x_k
-        Xobs <- dt_prev[is.na(dt_i[ , indx_k]), !indx_k]  # observed Xno_k for the missing on x_k
+        # Identify terminal nodes (l leafs) predictions (costum)
+        leafs <- data.frame(x_k = data4tree[, indx_k],
+                            lf = tree$where)        # terminal nodes
+        # Identify donor pool
+        donors <- lapply(unique(tree$where), function(s) data4tree$x_k[tree$where == s]) # Create donor pools (for each node, select the y values 
+                                                                               # that are observed (or previously imputed) for all cases in that node
+        # Identify prediction for donor pool
+        bbdonors <- lapply(donors, bbootstrap) # Bootstrap sample those donors
+        leaf_pred <- vapply(seq_along(bbdonors), function(s) sample(bbdonors[[s]], 1), FUN.VALUE = numeric(1))
+          # Get a simple sample from each donor pool that will be the imputed value for an observation that falls in a specific group
+          # (doubt: is a simple random sample with every value having equal prob enough?)
+        imputation_frame <- data.frame(leaf_id = unique(tree$where), # what value should be imputed if you fall in a leaf
+                                       leaf_pred)
+        # Xmis = values on X_no_k for observations with missing values on x_k
+        Xmis <- dt_aug[!ry, !indx_k]
+        Xmis_loca <- rpart.predict.leaves(tree, Xmis, type = "where") # location in the tree of Xmiss
         
         # Impute values
-        missings_frame <- rpart.predict.leaves(tree, Xobs, type = "where") # locate obs w/ missing x_K in a tree based on Xobs
-        for (obs in 1:length(missings_frame)) {
-          dt_prev[names(missings_frame)[obs], indx_k] <- imputation_frame[imputation_frame$leaf_id == missings_frame[obs], 2]
-          #imputation_frame %>% filter(leaf_id == missings_frame[obs]) %>% select(2)
+        for (obs in 1:length(Xmis_loca)) {
+          dt_aug[names(Xmis_loca)[obs], indx_k] <- imputation_frame[imputation_frame$leaf_id == Xmis_loca[obs], 2]
         }
       }
-      #print(dt_prev %>% filter(is.na(dt_i$yinc)) %>% select(1:7))
+      #print(dt_aug %>% filter(is.na(dt_i$yinc)) %>% select(1:7))
     }
-    return(dt_prev)
+    return(dt_aug)
   }
   
 # Imputation w/ MICE cart implementation ----------------------------------
-
+# for function details, see: https://github.com/stefvanbuuren/mice/blob/master/R/mice.impute.cart.R
   
+# Use
+  imp <- mice(dt_i, meth = 'cart', minbucket = 4)
+  imp_sets <- mice::complete(imp)
+  
+# Sections of mice code that are relevant
+    y <- dt_i$yinc
+      ry <- !is.na(y)
+      wy <- !ry
+    x <- dt_i[, 10:50]
+
+    cp <- 1e-04
+    minbucket <- 5
+    
+    xobs <- data.frame(x[ry, , drop = FALSE])
+    xmis <- data.frame(x[wy, , drop = FALSE])
+    yobs <- y[ry]
+
+    fit <- rpart(yobs ~ ., data = cbind(yobs, xobs), method = "anova", 
+                 control = rpart.control(minbucket = minbucket, cp = cp))
+    leafnr <- floor(as.numeric(row.names(fit$frame[fit$where, ])))          # leaf location of the training units
+    fit$frame$yval <- as.numeric(row.names(fit$frame))                      # combined with the following predict finds the location 
+    nodes <- predict(object = fit, newdata = xmis)                          # of the new data in the tree
+    donor <- lapply(nodes, function(s) yobs[leafnr == s])
+    impute <- vapply(seq_along(donor), function(s) sample(donor[[s]], 1), numeric(1))
+
+  # Mice does not do the bootstrap sampling and does not initialize with conditional means
--- notes/simulation_setup.md
+++ notes/simulation_setup.md
@@ -1,8 +1,22 @@
 # Simulation set up ideas
 
+## Overarching research question
+To what extent do the MI imputation methods considered grant statistical validity of the secondary data
+analysis when the data imputed is high-dimensional.
+
 ## Factorial Design
 * fixed: MAR + MCAR; sample size; set of auxiliary and junk variables
 * factors: % missing info; number of p p<<n, p \simeq n, p >> n; variable types?
+
+## Think about
+* Interactions in the true model: should the types of interactions be factors? (e.g. Doove et al 2014)
+	I do not think so as our research question is not related specifically
+	to how the different methods handle the presence of interactions. We are
+	more focused on which method achieves greater statistical validity of the
+	secondary analysis. It could be interesting to simply insert a variety of interaction
+	effects based on the results of Doove et al 2014: higher interaction effect size -> higher bias;
+	higher correlation between interacting variables -> better statistical performance of estiamtes;
+	double ordinal interactions are most well preserved, perfect double disordinal are the hardests.
 
 ## Evaluation criteria
 * estimates bias
