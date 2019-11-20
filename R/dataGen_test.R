8294ff0940888058c9aa95be8b6885fbbfa4de09
Edo <E.Costantini@tilburguniversity.edu>
2019-11-20 17:24:42 +0100

    updating BART implementation

--- R/dataGen_test.R
+++ R/dataGen_test.R
@@ -9,44 +9,83 @@
   f = function(x1,x2,x3,x4,x5) {10*sin(pi*x1*x2) + 20*(x3-.5)**2 + 10*x4 + 5*x5}
   pr = function(n,x) {rbinom(n, size = 1, prob = exp(x-2)/(1+exp(x-2))) }
   replace_na <- function(x, r) {replace(x, r==1, NA)}
-
-# dimensionality
-  n = 100 # sample size
-  p = 200 # number of predictors (relevant + junk)
   
-
-# Predictors
-  set.seed(20191113)
-  X <- as.data.frame(mvtnorm::rmvnorm(n, 
-                                      mean = rep(0, p), 
-                                      sigma=diag(rep(1, p))) )
-  round(apply(X, 2, var),1)
-  round(cor(X), 2) # just at random you get some correlation between certail predictors
-  
-# Noise
-  Z <- rnorm(n, 0, 1)
-
-# Dependent
-  # Model based on an example (Firedman) reported by Xu et al 2016 for the BART paper
-  y = f(X[,1],X[,2],X[,3],X[,4],X[,5]) + Z
-
-# Missingness
-  # Following a simple MAR mechanisms where the missingness on the
-  # first 5 predictors depends on the values of the 6th variable
-  R <- as.data.frame(split(rep( pr(n, X[, 6]), 6), 1:(1+ncol(X[,1:5])) )) # missingness for Y and first 5 Xs
-  yinc <- replace(y, R[,1]==1, NA)
-  Xinc <- X # initialize matrix for incomplete dataset
-  for (p in 1:ncol(X[,2:6])) {
-    Xinc[,p] <- replace(X[,p], R[,p]==1, NA)
+  missDataGen <- function(n, p){
+  ## Description
+    # The dependent variable depends on 5 variables (see f to understand how)
+    # Missing values are forces on y and those 5 predictors based on the values
+    # of a 6th variable. Hence, for this dataset, p >= 6
+    # Input: 
+    # - n: how many observations
+    # - p: how many variables (>=)
+    # Output: two datasets, one compelte and one with missing at random datasets 
+  ## Trial inputs
+    # n = 30 # sample size
+    # p = 7 # number of predictors (relevant + junk)
+    
+    # Predictors
+    X <- as.data.frame(mvtnorm::rmvnorm(n, 
+                                        mean = rep(0, p), 
+                                        sigma=diag(rep(1, p))) )
+    
+    # Noise for DV
+    Z <- rnorm(n, 0, 1)
+    
+    # Dependent
+    # Model based on an example (Firedman) reported by Xu et al 2016 for the BART paper
+    y = f(X[,1],X[,2],X[,3],X[,4],X[,5]) + Z
+    
+    # Missingness
+    # Following a simple MAR mechanisms where the missingness on the
+    # first 5 predictors depends on the values of the 6th variable
+    R <- as.data.frame(split(rep( pr(n, X[, 6]), 6), 1:(1+ncol(X[,1:5])) )) # missingness for Y and first 5 Xs
+    yinc <- replace(y, R[,1]==1, NA)
+    Xinc <- X # initialize matrix for incomplete dataset
+    for (p in 1:ncol(X[,2:6])) {
+      Xinc[,p] <- replace(X[,p], R[,p]==1, NA)
+    }
+    return(list(complete = cbind(y, X), 
+                incomplete = cbind(yinc, Xinc)))
   }
 
-# Missing data type
-  sum(rowSums(is.na(cbind(yinc, Xinc[, 1:5]))) != 0)/n # percetnage of cases with missing values 
-  mice::md.pattern(cbind(yinc, Xinc[, 1:5]))           # missing data patterns
-  mice::md.pairs(cbind(yinc, Xinc[, 1:5]))
-
-# Save data
-  out <- list(complete = cbind(y, X), 
-              incomplete = cbind(yinc, Xinc))
-  saveRDS(out,"../data/data_tryout.rds")
+# # Do it out of function (to save the data)
+# # dimensionality
+#   n = 100 # sample size
+#   p = 200 # number of predictors (relevant + junk)
+#   
+# 
+# # Predictors
+#   set.seed(20191113)
+#   X <- as.data.frame(mvtnorm::rmvnorm(n, 
+#                                       mean = rep(0, p), 
+#                                       sigma=diag(rep(1, p))) )
+#   round(apply(X, 2, var),1)
+#   round(cor(X), 2) # just at random you get some correlation between certail predictors
+#   
+# # Noise
+#   Z <- rnorm(n, 0, 1)
+# 
+# # Dependent
+#   # Model based on an example (Firedman) reported by Xu et al 2016 for the BART paper
+#   y = f(X[,1],X[,2],X[,3],X[,4],X[,5]) + Z
+# 
+# # Missingness
+#   # Following a simple MAR mechanisms where the missingness on the
+#   # first 5 predictors depends on the values of the 6th variable
+#   R <- as.data.frame(split(rep( pr(n, X[, 6]), 6), 1:(1+ncol(X[,1:5])) )) # missingness for Y and first 5 Xs
+#   yinc <- replace(y, R[,1]==1, NA)
+#   Xinc <- X # initialize matrix for incomplete dataset
+#   for (p in 1:ncol(X[,2:6])) {
+#     Xinc[,p] <- replace(X[,p], R[,p]==1, NA)
+#   }
+# 
+# # Missing data type
+#   sum(rowSums(is.na(cbind(yinc, Xinc[, 1:5]))) != 0)/n # percetnage of cases with missing values 
+#   mice::md.pattern(cbind(yinc, Xinc[, 1:5]))           # missing data patterns
+#   mice::md.pairs(cbind(yinc, Xinc[, 1:5]))
+# 
+# # Save data
+#   out <- list(complete = cbind(y, X), 
+#               incomplete = cbind(yinc, Xinc))
+#   saveRDS(out,"../data/data_tryout.rds")
   
--- R/try_bart_impute.R
+++ R/try_bart_impute.R
@@ -19,6 +19,17 @@ library(tidyverse)
     mice::md.pattern(dt_i)
     dim(dt_i)
     
+  # Create using datagen function
+  source("./dataGen_test.R")
+  set.seed(20191120)
+  dt <- missDataGen(n=100, p=500)
+  dt_c <- dt[[1]] # fully observed
+    mice::md.pattern(dt_c)
+    dim(dt_c)
+  dt_i <- dt[[2]] # with missings
+    mice::md.pattern(dt_i)
+    dim(dt_i)
+    
   # Define variables with missings
   K <- ncol(dt_i)-sum(tail(mice::md.pattern(dt_i),1) == 0) # number of variables needing imputation
   K_names <- names(which(colSums(apply(dt_i, 2, is.na)) != 0)) # select the names of these k variables
@@ -27,18 +38,104 @@ library(tidyverse)
 # Initialize by regression imputation
 #   using only the variables with missings and the variable predicting
 #   the missing (cannot use all variables).
+  # Initiliaze by stochastic regression imputation
+  #   using only the variables with missings and the variable predicting
+  #   the missing (cannot use all variables). Different aporaches could be
+  #   followed here, and since I'm following the approach of Reiter and similar
+  #   papers that are focused on creating sythetic datasets, there is not a
+  #   specific guideline on what to do in this approach.
+  print("Data initialization by MICE stochastic regression")
+  imp <- mice::mice(dt_i[,1:p_imod], method = "norm.nob", m = 1)
+  dt_init <- cbind(mice::complete(imp)[, names(dt_i) %in% K_names], dt_i[, !names(dt_i) %in% K_names])
   
-  imp <- mice::mice(dt_i[,1:7], method = "norm.predict", m = 1)
-  dt_imputed <- cbind(mice::complete(imp)[, names(dt_i) %in% K_names], dt_i[, !names(dt_i) %in% K_names])
-    dim(dt_imputed)
+  dt_aug <- dt_init # this data will be the previous dataset at the beginning of the loop and the new data at the end
 
 # Initialize by sample mean (as suggested in Xu et al 2016 example 1)
-  sample_means <- apply(dt_i[, names(dt_imputed) %in% K_names], 2, mean, na.rm = TRUE)
-  dt_imputed <- dt_i
+  print("Data initialization by sample mean")
+  sample_means <- apply(dt_i[, names(dt_i) %in% K_names], 2, mean, na.rm = TRUE)
+  dt_aug <- dt_i
   for (k in 1:K) {
-    dt_imputed <- dt_imputed %>% dplyr::mutate_at(vars(K_names[k]), ~replace(., is.na(.), sample_means[k]))
+    dt_aug <- dt_aug %>% dplyr::mutate_at(vars(K_names[k]), ~replace(., is.na(.), sample_means[k]))
   }
 
+# Sequential BART
+  iters <- 10
+  fast_diagnostic <- matrix(rep(NA, iters*7), ncol = 7)
+for(i in 1:iters) {
+  print(paste0("Iteration status: ", i, " of ", iters))
+  for (k in 1:K) {
+    print(paste0("Variable under imputation: ", K_names[k], " (", k, " of ", K, ")" ))
+    # Prep Data
+    indx_k <- names(dt_aug) %in% K_names[k]               # to index x_k
+    ry <- !is.na(dt_i[, K_names[k]])                      # response variable T = observed, F = NA
+    data4tree <- arrange_dt_x_k(dt_aug, K_names[k])[ry, ] # arrange and select Xobs (values of Xno_k for which X_k is observed)
+    Xobs <- data4tree[, -1]                               # after rearranging, the variable under imputation is always first
+      dim(Xobs)
+    yobs <- data4tree[, 1]
+      length(yobs)
+    Xmiss <- arrange_dt_x_k(dt_aug, K_names[k])[!ry, -1]     # observed and previously imputed / initialized values on all other variables
+      dim(Xmiss)
+    # Define tree type (classification or regression) based on nature of variable under imputation
+    #tt <- tree_type(data4tree$x_k)
+    
+    # Grow tree
+    set.seed(1234)
+    bt <- bart(Xobs, yobs, Xmiss, # using previously imputed dataset
+               # general setup
+               ntree = 50,
+               ndpost = 1e3,
+               keepevery = 1,
+               # prior specifications
+               # prior for error variance prior guess and degrees of freedom
+               sigest = 1,
+               sigdf = 3,      # nu
+               sigquant = .90, # q
+               # prior for the mean
+               k = 2,          # varaince scale parameter of the prior for mean
+               # prior for the trees
+               power = 2, base = .95,
+               verbose = F)
+    bt$yhat.test # for observation 1, column 1 has many ndpost/keeprecovery values.
+                 # each value is a sum of the mu value found by dropping that case in
+                 # the m trees that have been grown, + some error. Each value is then 
+                 # one sampled from the conditional distribution of Y given X, for a 
+                 # specific X.
+    bt$yhat.test.mean
+                 # The mean of what has been described for yhat.test is the estiamte 
+                 # of the Expected value of Y given that X takes the specific values
+                 # of one observation for which you want a prediction. Hence, this value
+                 # is a reasonable prediction of Y at a particular X
+    # Impute values
+    dt_aug[row.names(Xmiss), indx_k] <- bt$yhat.test.mean
+  }
+   new_impute <- dt_aug %>% filter(is.na(dt_i$yinc)) %>% select(1:7)
+   original <- dt_c %>% filter(is.na(dt_i$yinc)) %>% select(1:7)
+   fast_diagnostic[i,] <- colSums(new_impute - original)
+}
+  
+# Functions ---------------------------------------------------------------
+  
+# Data prep
+arrange_dt_x_k <- function(dt, var_name){
+  ## Description
+  # Input: 
+  # - dt: a complete dataset (initialized or augmented) containing a variable to be imputed and all imputation model variables
+  # - var_name: character vector containing only the name of variable under imputation
+  # Output: a dataset ready to be passed in a tree growing function with the under imputation variable in first column
+  ## Trial inputs
+  # dt <- mtcars
+  # var_name <- names(mtcars)[3]
+  indx_k <- names(dt) %in% var_name         # indexing obj: which variable is under imputation
+  indx_no_k <- !names(dt) %in% var_name     # indexing obj: all vairables EXCEPT the one under imputation 
+  Xno_k <- dt[, indx_no_k]                  # Use augmented Xno_k 
+  x_k <- dt[, indx_k]                       # Select variable under imputation
+  output <- data.frame(x_k = x_k, Xno_k)    # Combine the two for growing the tree
+  # (doubt: should I grow the tree only on the observed values?)
+  return(output)
+}
+  
+  
+  
 # Imputations
   X0 <- dt_c[, !names(dt_c) %in% K_names] # set of covariates fully observed
   X1p <- dt_c[, names(dt_c) %in% K_names] # set of covariates with potentially missing values (I also include y)
