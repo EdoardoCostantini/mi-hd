a0c822300db9d0565c2b3f36e9bd2b87834e1482
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-08-17 12:16:30 +0200

    cleanup: data generation functions

--- code/functions_genDt.R
+++ code/functions_genDt.R
@@ -2,8 +2,7 @@
 ### Porject:  Imputing High Dimensional Data
 ### Author:   Edoardo Costantini
 ### Created:  2020-06-23
-
-# data generation ---------------------------------------------------------
+### Modified: 2021-08-17
 
 # Experiment 1: Multivariate Data -----------------------------------------
 simData_exp1 <- function(cond, parms){
@@ -37,11 +36,6 @@ simData_exp1 <- function(cond, parms){
 
   return( as.data.frame( Z_sc ) )
 }
-
-# Use
-# Xy <- simData_exp1(conds[1, ], parms)
-# cor(Xy)[1:15, 1:15]
-# cov(Xy)[1:15, 1:15]
 
 # Missingness -------------------------------------------------------------
 
@@ -156,9 +150,6 @@ simData_lv <- function(parms, cond){
                scores_lv = scs_lv) )
 }
 
-# Use function
-# simData_lv()
-
 imposeMiss_lv <- function(dat_in, parms, cond){
   ## Description
   # Given a fully observed dataset and a param object containing 
@@ -186,222 +177,6 @@ imposeMiss_lv <- function(dat_in, parms, cond){
   # Result
   return( dat_out )
 }
-
-# MAR + MCAR
-imposeMiss_lv_MD <- function(dat_in, parms, cond, plot = FALSE){
-  ## Description
-  # Adds Matrix Desing missingness to the MAR
-  ## Example Inputs
-  # cond <- conds[1, ]
-  # cond$lv <- 50 # experimental
-  # dat_in <- simData_lv(parms, cond)
-  # plot = TRUE
-  
-  # Body
-  # Define non-response vector
-  dat_out <- dat_in$dat
-  for (i in 1:parms$zm_n) {
-    nR <- simMissingness(pm    = cond$pm,
-                         data  = dat_in$scores_lv,
-                         preds = parms$rm_x,
-                         type  = parms$missType,
-                         beta  = parms$auxWts)
-    
-    # Fill in NAs
-    dat_out[nR, parms$z_m_id[i]] <- NA
-  }
-  
-  # Assign observations to 1 of 6 situations
-  # situations <- paste0("s", 1:6)
-  # memo <- NULL
-  # for (i in 1:nrow(dat_out)) {
-  #   memo[[i]] <- sample(situations, 1)
-  # }
-  situations <- paste0("s", 1:6)
-  memo <- rep(paste0("s", 1:6), length.out = parms$n)
-  # sort(memo)
-  # Attribute Missingness Pattern (per situation)
-  missdesc <- list(s1 = c("C", "D"),
-                   s2 = c("B", "D"),
-                   s3 = c("B", "C"),
-                   s4 = c("A", "D"),
-                   s5 = c("A", "C"),
-                   s6 = c("A", "B"))
-  
-  # Define Variable-Block match
-  block_tot <- factor(x = c("Core", "A", "B", "C", "D"), 
-                      levels = c("Core", "A", "B", "C", "D"))
-  block_siz <- ncol(dat_out)/length(block_tot)
-  block_mem.1 <- list(L1 = rep("Core", 5),
-                      L2 = rep("A", 5),
-                      L3 = rep("Core", 5),
-                      L4 = rep("B", 5),
-                      L5 = rep("A", 5),
-                      L6 = rep("B", 5),
-                      L7 = rep("C", 5),
-                      L8 = rep("C", 5)
-  )
-  block_mem <- factor(unlist(block_mem.1), 
-                        levels = c("Core", "A", "B", "C", "D"))
-  block_tar <- block_siz - table(block_mem)
-  store <- list()
-  for (m in 1:length(block_tar)) {
-    store[[m]] <- rep(names(block_tar[m]), block_tar[m])
-  }
-
-  memb <- factor(c(as.character(block_mem), unlist(store)),
-                 levels = levels(block_tot))
-  table(memb)
-  # Impose Matrix Design missingness
-  for (s in 1:6) {
-    colIndex <- memb %in% missdesc[[s]]
-    rowIndex <- memo == situations[s]
-    dat_out[rowIndex, colIndex] <- NA
-  }
-  
-  # Visual Check
-  if(plot == TRUE){
-    # Order columns and rows to show the EVS matrix desing plot
-    dat_help <- dat_out[order(memo), order(memb)]
-    # plot the matrix
-    plot(as.matrix(!is.na(dat_help)), # requires "plot.matrix" pack
-         border = NA,
-         breaks = c(TRUE, FALSE),
-         col = c("black", "white"),
-         main = "Missing data pattern",
-         cex.lab = 1, # text size for axis labels
-         cex.axis = .5, # text size for ticks labels
-         las = 2, # orientation of ticks
-         y = "Observation ID",
-         xlab = "Variable ID")
-  }
-  
-  # Recast in original order
-  # dat_out <- dat_out[, OG_col_order]
-
-  # Result
-  return( dat_out )
-}
-
-
-# Experiment 3: Interactions ----------------------------------------------
-
-simData_int <- function(parms, cond){
-  # For internals
-  # cond <- conds[2,]
-  
-  # Generate covaraince matrix
-  # For paramters decisions, look back at
-  Sigma <- diag(cond$p - length(parms$blcky))
-  
-  # Block 1: highly correlated variables
-  blck1_indx <- parms$blck1 - length(parms$blcky)
-  Sigma[blck1_indx, ] <- parms$blck1_r
-  
-  # Block 2: not so highly correlated variables
-  blck2_indx <- parms$blck2 - length(parms$blcky)
-  Sigma[blck2_indx, ] <- parms$blck2_r
-  
-  # Block 3: uncorrelated variables
-  blck3_indx <- (1:ncol(Sigma))[-c(blck1_indx, blck2_indx)]
-  Sigma[blck3_indx, ] <- .01
-  
-  # Fix diagonal
-  diag(Sigma) <- 1
-  
-  # Make symmetric
-  Sigma[upper.tri(Sigma)] <- t(Sigma)[upper.tri(Sigma)]
-  
-  # Gen Predictors Variables
-  # Generic predictors
-  Z <- rmvnorm(n     = parms$n, 
-               mean  = rep(parms$item_mean, ncol(Sigma)), 
-               sigma = Sigma )
-  colnames(Z) <- paste0("z", 1:ncol(Z))
-  
-  # Gen y variables
-  if(cond$int_sub == FALSE){
-    y_pred   <- Z[, parms$lm_y_x]
-    y_b      <- rep(parms$b, ncol(y_pred))
-    y_sgn    <- t(y_b) %*% cov(y_pred) %*% y_b
-    y_sY     <- (y_sgn / cond$r2) - y_sgn
-    eps      <- rnorm(parms$n, mean = 0, sd = sqrt(y_sY))
-    y        <- y_pred %*% y_b + eps
-  }
-  if(cond$int_sub == TRUE){
-    y_inte <- apply(scale(Z[, parms$lm_y_i],
-                          center = parms$int_cen,
-                          scale = FALSE), 
-                    1, prod)
-    y_pred   <- cbind(Z[, parms$lm_y_x], y_inte)
-    y_b      <- rep(parms$b, ncol(y_pred))
-    y_sgn    <- t(y_b) %*% cov(y_pred) %*% y_b
-    y_sY     <- (y_sgn / cond$r2) - y_sgn
-    eps      <- rnorm(parms$n, mean = 0, sd = sqrt(y_sY))
-    y        <- y_pred %*% y_b + eps
-  }
-
-  # Combine data
-  yZ <- data.frame(y = y, Z)
-
-  return(yZ)
-}
-
-# yZ <- simData_int(parms = parms, cond = conds[1, ])
-# summary(lm(parms$frm, data = yZ))
-
-# yZ <- simData_int(parms = parms, cond = conds[2, ])
-# summary(lm(parms$frm_int, data = yZ))
-
-imposeMiss_int <- function(dat_in, parms, cond){
-  ## Description
-  # Given a fully observed dataset and a param object containing 
-  # info on the (latent) imposition mechanism, it returns a version of
-  # the original data with imposed missingness on all the items 
-  # indicated as target int parms$z_m_id
-  ## Example Inputs
-  # cond   <- conds[4, ]
-  # dat_in <- simData_int(parms, cond)
-  
-  # Body
-  # Define non-response vector
-  dat_out <- dat_in
-  
-  if(cond$int_rm == FALSE){
-    
-    for (i in parms$z_m_id) {
-      nR <- simMissingness(pm    = cond$pm,
-                           data  = dat_in,
-                           preds = parms$rm_x,
-                           type  = parms$missType)
-      
-      # Fill in NAs
-      dat_out[nR, i] <- NA
-    }
-  }
-  
-  if(cond$int_rm == TRUE){
-    int_term <- apply(scale(dat_in[, parms$rm_x],
-                            center = parms$int_cen,
-                            scale = FALSE),
-                      1, prod)
-    Z_pred   <- cbind(dat_in, int_term)
-    for (i in parms$z_m_id) {
-      nR <- simMissingness(pm    = cond$pm,
-                           data  = Z_pred,
-                           preds = parms$rm_x_int,
-                           type  = parms$missType,
-                           beta  = parms$auxWts_int)
-      
-      # Fill in NAs
-      dat_out[nR, i] <- NA
-    }
-  }
-  
-  # Result
-  return( dat_out )
-}
-
 
 # Experiment 4 ------------------------------------------------------------
 
