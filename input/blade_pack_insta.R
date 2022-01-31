c985d2ce2bd5fbf832c7fdac1d056def95c79140
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-01-31 15:18:00 +0100

    feat: add required-pacakges installation guide

---
+++ input/blade_pack_insta.R
@@ -0,0 +1,18 @@
+### Title:    Intialization for Blade Computing run
+### Author:   Edoardo Costantini
+### Created:  2020-05-16
+
+# Packges to install:
+
+  # 1. Install all packages you can
+  source("./init_general.R")
+  install.packages(pack_list)
+  
+  # 2. Install PcAux
+  install.packages("devtools")
+  devtools::install_github("PcAux-Package/PcAux/source/PcAux")
+  
+  # 3. Install blasso package on a Windows computer:
+  # Install w/ point and click interface. Use the gz.tar file in the package 
+  # folder in the impute_HD folder
+  
