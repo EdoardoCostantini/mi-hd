0aa1fc202d8b8fc4e32aa166efe8bd677c4835cc
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-04-19 14:35:08 +0200

    feat: script to concatenate exp 1.2 results

--- code/exp1.2_results.R
+++ code/exp1.2_results.R
@@ -2,25 +2,130 @@
 # Objective: Process result files to prepare analysis
 # Author:    Edoardo Costantini
 # Created:   2023-04-12
-# Modified:  2023-04-12
+# Modified:  2023-04-19
 # Notes: 
 
 rm(list = ls())
 source("./init_general.R")
 
-# Load data --------------------------------------------------------------------
+# Load R methods except MI-QP results ------------------------------------------
 
 filename <- c(
-    "exp1_2_simOut_20230408_1748" # 30 reps
+    "exp1_2_simOut_20230408_1748", # 30 reps with MI-QP time estimate
+    "exp1_2_simOut_20230415_0830" # All R methods expect MI-QP
+)[2]
+
+# Read R object
+out_Rmeth <- readRDS(paste0("../output/", filename, ".rds"))
+
+# Load IVEware results ---------------------------------------------------------
+
+filename <- c(
+    "exp1_2_simOut_20230417_1051"
 )[1]
 
 # Read R object
-out <- readRDS(paste0("../output/", filename, ".rds"))
-out$parms
-out$conds
-out$session_info
+out_IVEware <- readRDS(paste0("../output/", filename, ".rds"))
 
-# Time Analyses -----------------------------------------------------------
+# Load MI-QP results -----------------------------------------------------------
+
+filename <- c(
+    "exp1_2_simOut_20230419_0957"
+)[1]
+
+# Read R object
+out_MIQP <- readRDS(paste0("../output/", filename, ".rds"))
+
+# Final file name --------------------------------------------------------------
+
+filename <- paste0("exp1_2_simOut_", format(Sys.time(), "%Y%m%d_%H%M"))
+
+# Concatenate results ----------------------------------------------------------
+
+# Add IVEware to Rmeth
+
+for (i in 1:(out_Rmeth$parms$dt_rep)) { # for every repetition
+    # i <- 1
+    for (j in 1:(nrow(out_Rmeth$conds))) { # for every condition
+        # j <- 1
+        for (h in 2:length(out_Rmeth[[i]][[j]])) {
+            # h <- 2
+            multi_dim <- length(dim(out_Rmeth[[i]][[j]][[h]])) == 2
+            if (multi_dim) {
+                colnames1 <- colnames(out_Rmeth[[i]][[j]][[h]])
+                colnames2 <- colnames(out_IVEware[[i]][[j]][[h]])
+                colindex <- !colnames2 %in% colnames1
+                out_Rmeth[[i]][[j]][[h]] <- cbind(
+                    out_Rmeth[[i]][[j]][[h]],
+                    out_IVEware[[i]][[j]][[h]][, colindex,
+                        drop = FALSE
+                    ]
+                )
+            } else {
+                names1 <- names(out_Rmeth[[i]][[j]][[h]])
+                names2 <- names(out_IVEware[[i]][[j]][[h]])
+                namesindex <- !names2 %in% names1
+                out_Rmeth[[i]][[j]][[h]] <- c(
+                    out_Rmeth[[i]][[j]][[h]],
+                    out_IVEware[[i]][[j]][[h]][namesindex]
+                )
+            }
+        }
+    }
+}
+
+# Add MI-QP
+
+for (i in 1:(out_Rmeth$parms$dt_rep)) { # for every repetition
+    # i <- 1
+    for (j in 1:nrow(out_MIQP$conds)) { # for every condition
+        # j <- 4
+        for (h in 2:length(out_Rmeth[[i]][[j]])) {
+            # h <- 2
+            multi_dim <- length(dim(out_Rmeth[[i]][[j]][[h]])) == 2
+            if (multi_dim) {
+                colnames1 <- colnames(out_Rmeth[[i]][[j]][[h]])
+                colnames2 <- colnames(out_MIQP[[i]][[j]][[h]])
+                colindex <- !colnames2 %in% colnames1
+                out_Rmeth[[i]][[j]][[h]] <- cbind(
+                    out_Rmeth[[i]][[j]][[h]],
+                    out_MIQP[[i]][[j]][[h]][, colindex,
+                        drop = FALSE
+                    ]
+                )
+            } else {
+                names1 <- names(out_Rmeth[[i]][[j]][[h]])
+                names2 <- names(out_MIQP[[i]][[j]][[h]])
+                namesindex <- !names2 %in% names1
+                out_Rmeth[[i]][[j]][[h]] <- c(
+                    out_Rmeth[[i]][[j]][[h]],
+                    out_MIQP[[i]][[j]][[h]][namesindex]
+                )
+            }
+        }
+    }
+}
+
+# Update the parms objects -----------------------------------------------------
+
+# Start from the first file's parms object
+parms <- out_Rmeth$parms
+
+# Create new vector of methds
+new_meth <- rep(TRUE, length(parms$meth_sel))
+
+# Give it good names
+names(new_meth) <- names(parms$meth_sel)
+
+# Update internal objects 
+parms$meth_sel <- new_meth
+parms$methods <- names(new_meth)
+
+# Create final output object
+out <- out_Rmeth
+out$parms <- parms
+
+# Time Analyses ----------------------------------------------------------------
 
   out_time <- sapply(1:nrow(out$conds),
                      res_sem_time,
