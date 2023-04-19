61e63d701c8aab37838f7d314f0795f4230baa3f
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-04-19 14:35:21 +0200

    feat: add plotting for CI and CIW for exp 1.2

--- code/exp1.2_analysis.R
+++ code/exp1.2_analysis.R
@@ -9,7 +9,8 @@ rm(list = ls())
 source("./init_general.R")
 
 # Read results from the combined results
-res <- readRDS("../output/exp1_2_simOut_20230408_1748_res.rds")
+res <- readRDS("../output/exp1_2_simOut_20230415_0830_res.rds")
+res <- readRDS("../output/exp1_2_simOut_20230419_1102_res.rds")
 
 # Change names of methods if required
 levels(res$methods) <- str_replace(levels(res$methods), "blasso", "BLasso")
@@ -20,7 +21,7 @@ levels(res$methods) <- str_replace(levels(res$methods), "MI-OP", "MI-OR")
 levels(res$methods) <- str_replace(levels(res$methods), "stepFor", "MI-SF")
 
 # Which plot to plot
-p_grep <- c("50", "500")[2]
+p_grep <- c("50", "500")[1]
 
 # Plot Sizes Decisions
 
@@ -155,9 +156,218 @@ ggsave(
     pf
 )
 
+# Confidence interval coverage ------------------------------------------------
+
+x <- 2 # Confidence intervals
+methods_sel <- levels(output_sem$methods) # [-8]
+
+# SE for threshold
+ci_lvl <- .95
+dt_reps <- 500
+SEp <- sqrt(ci_lvl * (1 - ci_lvl) / dt_reps)
+low_thr <- (.95 - SEp * 2) * 100
+hig_thr <- (.95 + SEp * 2) * 100
+vline_burton <- c(low_thr, hig_thr)
+vline_vanBuu <- c(90, 99)
+xci_breaks <- sort(c(80, vline_vanBuu, 95, round(vline_burton, 1), 100))
+
+pf <- output_sem %>%
+  filter(
+    analysis == unique(analysis)[x],
+    grepl(p_grep, cond),
+    variable %in% c("Min", "Mean", "Max"),
+    methods %in% methods_sel
+  ) %>%
+  # Drop pm = ** as we are plotting only one value for this condition
+  mutate(cond = fct_relabel(
+    cond,
+    str_replace,
+    " pm = [0-9]\\.[0-9]", ""
+  )) %>%
+  mutate(methods = fct_relabel(
+    methods,
+    str_replace,
+    "-la", ""
+  )) %>%
+  # Main Plot
+  ggplot(data = ., aes(
+    y = methods,
+    x = value,
+    shape = variable
+  )) +
+  geom_point(size = 1.75, show.legend = FALSE) +
+  geom_line(aes(group = methods),
+    size = .25
+  ) +
+  # Grid
+  facet_grid(
+    rows = vars(factor(parm,
+      levels = unique(parm)
+    )),
+    cols = vars(cond)
+  ) +
+  geom_vline(
+    data = data.frame(
+      xint = 95,
+      analysis = "CI coverage"
+    ),
+    linetype = "solid",
+    size = .15,
+    aes(
+      xintercept = xint,
+      lty = paste0("nominal level")
+    )
+  ) + 
+
+  # Format
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(
+    labels = as.character(round(xci_breaks / 100, 2)),
+    breaks = xci_breaks
+  ) +
+  coord_cartesian(xlim = c(min(xci_breaks), max(xci_breaks))) +
+  scale_shape_manual(values = c("I", "I", "I")) +
+  # scale_linetype_manual(values = c("longdash", "dashed", "solid")) +
+  # guides(linetype = guide_legend(override.aes = list(size = c(.20, .20, .15)))) +
+  labs( # title = label_parm[x],
+    x = NULL,
+    y = NULL,
+    linetype = NULL,
+    shape = NULL
+  ) +
+  theme(
+    panel.background = element_rect(
+      fill = NA,
+      color = "gray"
+    ),
+    panel.grid.major = element_line(
+      color = "gray",
+      size = 0.175,
+      linetype = 1
+    ),
+    axis.ticks = element_blank(),
+    legend.key = element_rect(
+      colour = "gray",
+      fill = NA,
+      size = .15
+    ),
+    legend.position = "bottom",
+    text = element_text(
+      family = plot_text_family,
+      face = plot_text_face,
+      size = plot_text_size
+    ),
+    axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )
+  )
+
+pf
+
+plot_name <- paste0(
+  "../output/graphs/exp1.2_", "CI", "_",
+  as.numeric(p_grep) * 100, "revision.pdf"
+)
+ggsave(
+  file = plot_name,
+  width = gp_width, height = gp_height * .65,
+  units = "cm",
+  pf
+)
+
+# Confidence Interval Width ---------------------------------------------------
+
+# SE for threshold
+dt_reps <- 500
+
+# Bias
+x <- 3 # CIW
+methods_sel <- levels(output_sem$methods)[-c(12)]
+
+pf <- output_sem %>%
+    filter(
+        analysis == unique(analysis)[x],
+        grepl(p_grep, cond),
+        variable %in% c("Mean"),
+        methods %in% methods_sel
+    ) %>%
+    # Drop pm = ** as we are plotting only one value for this condition
+    mutate(cond = fct_relabel(
+        cond,
+        str_replace,
+        " pm = [0-9]\\.[0-9]", ""
+    )) %>%
+    mutate(methods = fct_relabel(
+        methods,
+        str_replace,
+        "-la", ""
+    )) %>%
+    # Main Plot
+    ggplot(data = ., aes(
+        y = methods,
+        x = value
+    )) +
+    geom_point(size = 1.75) +
+    # Grid
+    facet_grid(
+        rows = vars(factor(parm,
+            levels = unique(parm)
+        )),
+        cols = vars(cond)
+    ) +
+    coord_cartesian(xlim = c(0, 10)) +
+
+    # Format
+    scale_y_discrete(limits = rev) +
+    labs( # title = label_parm[x],
+        x = NULL,
+        y = NULL,
+        linetype = NULL
+    ) +
+    theme(
+        panel.background = element_rect(
+            fill = NA,
+            color = "gray"
+        ),
+        panel.grid.major = element_line(
+            color = "gray",
+            size = 0.15,
+            linetype = 1
+        ),
+        legend.key = element_rect(
+            colour = "gray",
+            fill = NA,
+            size = .15
+        ),
+        text = element_text(
+            family = plot_text_family,
+            face = plot_text_face,
+            size = plot_text_size
+        ),
+        axis.ticks = element_blank(),
+        legend.position = "none"
+    )
+
+pf
+
+plot_name <- paste0(
+    "../output/graphs/exp1.2_", "ciw", "_",
+    as.numeric(p_grep) * 100, "revision.pdf"
+)
+
+ggsave(
+    file = plot_name,
+    width = gp_width, height = gp_height * .65,
+    units = "cm",
+    pf
+)
+
 # Time plot ---------------------------------------------------------------
 
 dt_outtime <- readRDS("../output/exp1_2_simOut_20230408_1748_time.rds")
 
 round(dt_outtime, 1)
 round(dt_outtime/60, 0)
+
